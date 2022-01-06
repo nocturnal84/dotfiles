1594512e967aa724192900949391661ead80c14b
nocturnal84 <96955486+nocturnal84@users.noreply.github.com>
2022-01-05 23:39:40 -0500

    Add a module for cmus

---
+++ polybar/scripts/cmus.sh
@@ -0,0 +1,33 @@
+while getopts "qnpt" arg; do
+    case $arg in
+        q)
+            status=$(cmus-remote -Q | awk '/^status/{ s=$2 } /^tag title/{ $1=""; $2=""; gsub(/^[ ]+/, "", $0); t=$0 } /^tag artist/{ $1=""; $2=""; gsub(/^[ ]+/, "", $0); a=$0 } END { print s"::"t"::"a }')
+            if [[ $status =~ ^([^\:]+)\:\:([^\:]+)\:\:([^\:]+)$ ]]; then
+                # Matches status::title::artist
+                icon=""
+                case ${BASH_REMATCH[1]} in
+                    playing)
+                        icon="﫛  "
+                    ;;
+                esac
+                label="${BASH_REMATCH[3]} - ${BASH_REMATCH[2]}"
+                if [[ ${#label} > 75 ]]; then
+                    label=$(echo "$label" | awk '{ print substr($0, 1, 75) "..." }')
+                fi
+                echo "${icon}$label"
+
+            else
+                echo "$status"
+            fi
+            ;;
+        t)
+            cmus-remote -u
+            ;;
+        n)
+            cmus-remote -n
+            ;;
+        p)
+            cmus-remote -r
+            ;;
+    esac
+done
