45c68bc5ff5eb787775f7b16fcfabdfcaa2996bf
nocturnal84 <96955486+nocturnal84@users.noreply.github.com>
2022-01-18 12:38:55 -0500

    Prevent update-package script from closing automatically in case there's work to do

--- polybar/scripts/update-packages.sh
+++ polybar/scripts/update-packages.sh
@@ -3,3 +3,5 @@
 package="\033[1;34m\033[0m" 
 echo -e "$package \033[1mUpdating packages on \033[1;31m$HOSTNAME\033[0m $package\033[1m...\033[0m"
 sudo pacman -Syyu
+
+read -p "Press Enter to continue..."
